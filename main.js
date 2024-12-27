604a02afe3fab4ad6c3db0e56fc02424838b23e8
agusSantos23 <131393036+agusSantos23@users.noreply.github.com>
2024-12-27 23:13:48 +0100

    FIX build

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { app, BrowserWindow } from 'electron'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const createWindow = () => {
   const window = new BrowserWindow({
@@ -7,7 +11,7 @@ const createWindow = () => {
     autoHideMenuBar: true
   })
 
-  window.loadFile('index.html')
+  window.loadFile(path.join(__dirname, 'index.html'))
 }
 
 app.whenReady().then(createWindow)
